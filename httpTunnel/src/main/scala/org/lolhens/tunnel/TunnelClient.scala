b7087999ec728d24d8092113178f462b5ac9a5d1
LolHens <pierrekisters@gmail.com>
2017-08-14 13:42:55 +0200

    maybe this is better?

--- httpTunnel/src/main/scala/org/lolhens/tunnel/TunnelClient.scala
+++ httpTunnel/src/main/scala/org/lolhens/tunnel/TunnelClient.scala
@@ -4,11 +4,10 @@ import java.util.UUID
 
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.stream.ThrottleMode
 import akka.stream.scaladsl.{Flow, Sink, Source, Tcp}
-import akka.stream.{OverflowStrategy, ThrottleMode}
 import akka.util.ByteString
 import monix.execution.Scheduler.Implicits.global
-import monix.execution.atomic.Atomic
 
 import scala.concurrent.duration._
 import scala.io.StdIn
@@ -31,7 +30,7 @@ object TunnelClient extends Tunnel {
         .to(Sink.foreach { tcpConnection =>
           val id = UUID.randomUUID().toString
 
-          val lastReq = Atomic(None: Option[HttpRequest])
+          //val lastReq = Atomic(None: Option[HttpRequest])
 
           val httpConnection = Flow[ByteString]
             .map(data => HttpRequest(
@@ -40,80 +39,42 @@ object TunnelClient extends Tunnel {
               headers = List(headers.Host(tunnelServer)),
               entity = HttpEntity.Strict(ContentTypes.`application/octet-stream`, data)
             ))
-            .map { e => lastReq.set(Some(e)); e }
+            //.map { e => lastReq.set(Some(e)); e }
             .via(Http().outgoingConnection(server.host.toString(), server.port))
             .flatMapConcat {
               case HttpResponse(StatusCodes.OK, _, entity: ResponseEntity, _) => entity.dataBytes
               case r =>
-                println("ERR3: " + r + "   " + lastReq.get)
+                //println("ERR3: " + r + "   " + lastReq.get)
                 Source.empty[ByteString]
             }
 
-          val httpOutBuffer = Atomic(ByteString.empty)
-          val httpInBuffer = Atomic(ByteString.empty)
-
-          //val (httpResponseSignalInlet, httpResponseSignalOutlet1, httpResponseSignalOutlet2) = coupling2[Unit]
-          //val (tcpResponseSignalInlet, tcpResponseSignalOutlet) = coupling[Unit]
           val (httpResponseSignalInlet, httpResponseSignalOutlet) = coupling[Unit]
 
-          /*httpResponseSignalOutlet1
-            .map(_ => httpInBuffer.getAndSet(ByteString.empty))
-            .map { e => println("RES " + time + " " + id + " " + e.size + ":" + toBase64(e).utf8String); e }
-            .via(tcpConnection.flow)
-            .map { e => println("REQ " + time + " " + id + " " + e.size + ":" + toBase64(e).utf8String); e }
-            .filter(_.nonEmpty)
-            .map { data => httpOutBuffer.transform(_ ++ data); data }
-            .map(_ => ())
-            .to(tcpResponseSignalInlet)
-            .run()*/
-
-          Flow[Unit]
-            .buffer(4, OverflowStrategy.dropNew)
-            .throttle(1, 5.millis, 1, ThrottleMode.Shaping)
-            .map(_ => httpInBuffer.getAndSet(ByteString.empty))
-            .map { e => println("RES " + time + " " + id + " " + e.size + ":" + toBase64(e).utf8String); e }
-            .via(tcpConnection.flow)
-            .map { e => println("REQ " + time + " " + id + " " + e.size + ":" + toBase64(e).utf8String); e }
-            .filter(_.nonEmpty)
-            .map { data => httpOutBuffer.transform(_ ++ data); data }
-            .map(_ => ())
-            .join {
-              Flow[Unit]
-                .merge(Source.tick(0.millis, 500.millis, ()))
-                .merge(
-                  httpResponseSignalOutlet
-                  .flatMapMerge(2, _ =>
-                    Source.tick(100.millis, 200.millis, ()).take(25)
-                    .merge(Source.tick(10.millis, 10.millis, ()).take(20))
-                  )
-                )
-                .buffer(4, OverflowStrategy.dropNew)
-                .throttle(1, 10.millis, 1, ThrottleMode.Shaping)
-                .map(_ => httpOutBuffer.transformAndExtract(data => (data.take(maxHttpPacketSize), data.drop(maxHttpPacketSize))))
-                .via(httpConnection)
-                .filter(_.nonEmpty)
-                .map { data => httpInBuffer.transform(_ ++ data); data }
-                .map { e => println("received " + e.size) }
-                .map(_ => ())
-                .alsoTo(httpResponseSignalInlet)
-            }
-            .run()
-
-          /*httpResponseSignalOutlet2
-            .flatMapMerge(2, _ =>
-              Source.tick(0.millis, 50.millis, ()).take(10)
-                //.merge(Source.tick(10.millis, 5.millis, ()).take(50))
-            )
-            .merge(tcpResponseSignalOutlet)
-            .merge(Source.tick(0.millis, 200.millis, ()))
-            .map(_ => httpOutBuffer.transformAndExtract(data => (data.take(maxHttpPacketSize), data.drop(maxHttpPacketSize))))
+          Flow[ByteString]
+            .map { e => println("signal1 " + id); e }
+            .merge(httpResponseSignalOutlet.map(_ => ByteString.empty).map { e => println("signal2 " + id); e })
+            .flatMapMerge(2, e => Source.single(e).concat(
+              Source.tick(10.millis, 10.millis, ByteString.empty).take(20)
+                .concat(Source.tick(0.millis, 100.millis, ByteString.empty).take(20))
+                .concat(Source.tick(0.millis, 500.millis, ByteString.empty).take(10))
+            ))
+            .batch(Int.MaxValue, e => e)((last, e) => last ++ e)
+            .throttle(1, 10.millis, 1, ThrottleMode.Shaping)
+            .map { e => println("signal " + id); e }
+            .keepAlive(2000.millis, () => ByteString.empty)
+            //.map { e => println("signal " + id); e }
             .via(httpConnection)
             .filter(_.nonEmpty)
-            .map { data => httpInBuffer.transform(_ ++ data); data }
-              .map{e => println("received " + e.size)}
-            .map(_ => ())
-            .to(httpResponseSignalInlet)
-            .run()*/
+            .map { e => println("received " + e.size); e }
+            .alsoTo(Flow[ByteString].map(_ => ()).to(httpResponseSignalInlet))
+            .join {
+              Flow[ByteString]
+                .map { e => println("RES " + time + " " + id + " " + e.size + ":" + toBase64(e).utf8String); e }
+                .via(tcpConnection.flow)
+                .filter(_.nonEmpty)
+                .map { e => println("REQ " + time + " " + id + " " + e.size + ":" + toBase64(e).utf8String); e }
+            }
+            .run()
 
         }).run().map { e => println(e); e }
 
