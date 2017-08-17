5c4d0dbaa3da1de8db3c6be9e681f9cce20c5a54
LolHens <pierrekisters@gmail.com>
2017-08-17 11:31:44 +0200

    better compression

--- httpTunnel/src/main/scala/org/lolhens/tunnel/LZ4Compressor.scala
+++ httpTunnel/src/main/scala/org/lolhens/tunnel/LZ4Compressor.scala
@@ -1,6 +1,7 @@
 package org.lolhens.tunnel
 
 import akka.util.ByteString
+import monix.execution.atomic.Atomic
 import net.jpountz.lz4.LZ4Factory
 import scodec.bits._
 import scodec.interop.akka._
@@ -11,24 +12,36 @@ object LZ4Compressor {
   private lazy val compressor = factory.fastCompressor()
   private lazy val decompressor = factory.fastDecompressor()
 
+  private val noCompressionMarker = bin"10000000".toByteVector
+
   def compress(data: ByteVector): ByteVector = {
     val compressed = ByteVector.view(compressor.compress(data.toArray))
-    if (compressed.size < data.size)
-      ByteVector.fromInt(data.size.toInt) ++ compressed
+    val size = data.size.toInt
+    if (compressed.size < data.size && size >= 0)
+      ByteVector.fromInt(size) ++ compressed
     else
-      ByteVector.fromInt(-1) ++ data
+      noCompressionMarker ++ data
   }
 
-  def decompress(data: ByteVector): ByteVector = {
-    val (sizeBytes, compressed) = data.splitAt(4)
-    val size = sizeBytes.toInt()
-    if (size == -1)
-      compressed
-    else
-      ByteVector.view(decompressor.decompress(compressed.toArray, size))
-  }
+  def decompress(data: ByteVector): ByteVector =
+    if (data.take(1) === noCompressionMarker)
+      data.drop(1)
+    else {
+      val size = data.take(4).toInt()
+      ByteVector.view(decompressor.decompress(data.drop(4).toArray, size))
+    }
 
-  def compress(data: ByteString): ByteString = compress(data.toByteVector).toByteString
+  //val ratio = Atomic((0L, 0L))
+
+  def compress(data: ByteString): ByteString = {
+    val r = compress(data.toByteVector).toByteString
+    /*ratio.transform {
+      case (uncomp, comp) =>
+        println(comp.toDouble / uncomp.toDouble)
+        (uncomp + data.size, comp + r.size)
+    }*/
+    r
+  }
 
   def decompress(data: ByteString): ByteString = decompress(data.toByteVector).toByteString
 }
